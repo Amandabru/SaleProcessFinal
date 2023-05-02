7d26fe9923ec612905c61159d6ca292e185080ba
Amanda Brundin <abrundin@kth.se>
2023-05-02 17:44:13 +0200

    tests

---
+++ src/test/java/se/kth/iv1350/saleProcess/Integration/ExternalInventorySystemTest.java
@@ -0,0 +1,16 @@
+package se.kth.iv1350.saleProcess.Integration;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExternalInventorySystemTest {
+
+    @Test
+    void getItem() {
+    }
+
+    @Test
+    void updateInventory() {
+    }
+}
---
+++ src/test/java/se/kth/iv1350/saleProcess/model/LineItemTest.java
@@ -0,0 +1,16 @@
+package se.kth.iv1350.saleProcess.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LineItemTest {
+
+    @Test
+    void increaseQuantity() {
+    }
+
+    @Test
+    void decreaseQuantity() {
+    }
+}
