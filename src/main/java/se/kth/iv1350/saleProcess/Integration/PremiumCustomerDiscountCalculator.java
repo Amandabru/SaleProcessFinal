eb7a236640936d5a956905cd33f634f61211a072
Hanna Snarberg <hannasnaris@gmail.com>
2023-05-15 14:21:25 +0200

    Commented the premium discount class

--- src/main/java/se/kth/iv1350/saleProcess/Integration/PremiumCustomerDiscountCalculator.java
+++ src/main/java/se/kth/iv1350/saleProcess/Integration/PremiumCustomerDiscountCalculator.java
@@ -3,8 +3,18 @@ package se.kth.iv1350.saleProcess.Integration;
 import se.kth.iv1350.saleProcess.model.Sale;
 import se.kth.iv1350.saleProcess.utils.Amount;
 
+/**
+ * Class that implements the <code>DiscountCalculator</code> interface and that calculates
+ * discount for premium customers.
+ */
 public class PremiumCustomerDiscountCalculator implements DiscountCalculator{
 
+    /**
+     * Premium discount is calculated. The number of bought items decides the
+     * percentage of the discount.
+     * @param sale the <code>sale</code> object representing the whole sale.
+     * @return the premium discount amount for the sale.
+     */
     @Override
     public Amount calculateDiscount(Sale sale) {
         Amount discount = new Amount();
