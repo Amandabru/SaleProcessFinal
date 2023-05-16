5790a748a634133499b340551c07180bb91ef36a
Hanna Snarberg <hannasnaris@gmail.com>
2023-05-16 15:53:40 +0200

    One comment done

--- src/main/java/se/kth/iv1350/saleProcess/Integration/CompositeDiscountCalculator.java
+++ src/main/java/se/kth/iv1350/saleProcess/Integration/CompositeDiscountCalculator.java
@@ -6,8 +6,18 @@ import se.kth.iv1350.saleProcess.utils.Amount;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Composite that implements <code>discountCalculator</code> and handles the discount calculation.
+ */
 public class CompositeDiscountCalculator implements DiscountCalculator{
     private List<DiscountCalculator> discountAlgorithmList = new ArrayList<>();
+
+    /**
+     * Calculates the discount by applying the discount algorithm that results in the largest discount.
+     * @param sale the current sale that is to be discounted.
+     * @param customerId the customer id of the customer.
+     * @return the largest possible discount for the sale.
+     */
     @Override
     public Amount calculateDiscount(Sale sale, Integer customerId){
         Amount bestDiscount = new Amount();
